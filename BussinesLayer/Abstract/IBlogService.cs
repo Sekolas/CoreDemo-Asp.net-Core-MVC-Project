a9630c8bbdbd514576f0c4f0c639e66ae36e945e
Serkan Kemer <115104251+Sekolas@users.noreply.github.com>
2024-10-12 03:19:09 +0300

    update-3

---
+++ BussinesLayer/Abstract/IBlogService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinesLayer.Abstract
+{
+	public interface IBlogService
+	{
+		void AddBlog(Blog category);
+		void RemoveBlog(Blog category);
+		void BlogUpdate(Blog category);
+		List<Blog> GetList();
+		Blog GetById (int id);
+	}
+}
---
+++ BussinesLayer/Concrete/BlogManager.cs
@@ -0,0 +1,45 @@
+﻿using BussinesLayer.Abstract;
+using DataAccesLayer.abstracct;
+using EntityLayer.concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinesLayer.Concrete
+{
+	public class BlogManager : IBlogService
+	{
+		IBlogDal _blogdal;
+
+        public BlogManager(IBlogDal blogdal)
+        {
+            _blogdal=blogdal;
+        }
+        public void AddBlog(Blog category)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void BlogUpdate(Blog category)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Blog GetById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Blog> GetList()
+		{
+			return _blogdal.GetListAll();
+		}
+
+		public void RemoveBlog(Blog category)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
